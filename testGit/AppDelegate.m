6e4e8514e84b838f7e7c66731ab593f90fa4aaba
Kevin <Billy@billytekimacbook-pro.local>
2016-02-22 16:35:53 +0800

    test

--- testGit/AppDelegate.m
+++ testGit/AppDelegate.m
@@ -17,7 +17,7 @@
 
 - (BOOL)application:(UIApplication *)application didFinishLaunchingWithOptions:(NSDictionary *)launchOptions {
     // Override point for customization after application launch.
-    NSLog(@"Hello World!!");
+    NSLog(@"Hello World!!");//fghfgh
     return YES;
 }
 
